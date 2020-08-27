4d04e73daad46a4ca3cc34398a0225e06e9c2e30
Jérémie Bannwarth <jban039@aucklanduni.ac.nz>
2020-08-27 13:52:28 +1200

    Implement SolveRKF45, variable step-size RK algo

--- chapter1/numerical_solvers.py
+++ chapter1/numerical_solvers.py
@@ -5,6 +5,7 @@ Based on 'Orbital Mechanics for Engineering Students' Section 1.8
 Written by: J.X.J. Bannwarth
 """
 import numpy as np
+from copy import deepcopy
 
 
 def RK14(y, t, f, h, order=4):
@@ -64,7 +65,7 @@ def RK14(y, t, f, h, order=4):
 
 
 def SolveRK14(f, y0, tMax=10.0, h=0.01, order=4):
-    """Use Runge-Kutta algorithm numerically solve ODE.
+    """Use Runge-Kutta algorithm to numerically solve ODE.
 
     Parameters
     ----------
@@ -97,6 +98,92 @@ def SolveRK14(f, y0, tMax=10.0, h=0.01, order=4):
     for n in range(1, ts.shape[0]):
         ys[n, :] = RK14(ys[n - 1, :], ts[n - 1], f, h, order)
 
+    return (ys, ts)
+
+
+def SolveRKF45(f, y0, tSpan=np.array([0., 10.0]), tol=1.e-8):
+    """Use variable step-size Runge-Kutta algorithm to numerically solve ODE.
+
+    Parameters
+    ----------
+    f : function
+        Function to compute the derivative of y.
+    y0 : numpy.array
+        Initial value of state.
+    tSpan : numpy.array
+        Span of time to solve for.
+    tol : float
+        Allowable truncation error.
+
+    Returns
+    -------
+    ys : numpy.array
+        State time response.
+    ts : numpy.array
+        Time vector
+    """
+    # Constants
+    A = np.array([0., 1./4., 3./8., 12./13., 1., 1./2.])
+    B = np.array([[0., 0., 0., 0., 0.],
+                  [1./4., 0., 0., 0., 0.],
+                  [3./32., 9/32., 0., 0., 0.],
+                  [1932./2197., -7200./2197., 7296./2197., 0., 0.],
+                  [439./216., -8., 3680./513., -845./4104., 0.],
+                  [-8./27., 2., -3544./2565., 1859./4104., -11./40.]])
+    C4 = np.array([25./216., 0., 1408./2565., 2197./4104., -1./5., 0.])
+    C5 = np.array([16./135., 0., 6656./12825., 28561./56430., -9./50., 2./55.])
+
+    nStates = y0.shape[0]
+    t0 = tSpan[0]
+    tMax = tSpan[1]
+
+    # Solve for each time-step
+    ts = [0]
+    ys = [y0]
+    h = (tMax - t0)/100.
+
+    t = ts[-1]
+    y = ys[-1]
+    while ts[-1] < tMax:
+        tTilde = t + A * h
+        fTilde = np.zeros((6, nStates))
+        yTilde = np.zeros((6, nStates))
+
+        # Stage 1
+        fTilde[0, :] = f(t, y)
+
+        # Stage 2+
+        for m in range(1, fTilde.shape[0]):
+            yTilde[m, :] = y + h * np.dot(B[m, 0:m], fTilde[0:m, :])
+            fTilde[m, :] = f(tTilde[m], yTilde[m, :])
+
+        Phi4 = np.dot(C4, fTilde)
+        Phi5 = np.dot(C5, fTilde)
+        yN4 = y + h * Phi4
+        yN5 = y + h * Phi5
+
+        # Truncation error
+        err = np.max(np.abs(yN5 - yN4))
+
+        # Maximum truncation error
+        yMax = np.max(np.abs(y))
+        errAllow = tol*np.max([yMax, 1.0])
+
+        # Change in step size
+        delta = (errAllow/(err+np.finfo(float).eps))**0.2
+
+        # Save if error is in bounds
+        if err <= errAllow:
+            h = np.min([h, tMax-t])
+            t += h
+            y += h*Phi5
+            ts.append(deepcopy(t))
+            ys.append(deepcopy(y))
+
+        # Update time step
+        h = np.min([delta*h, 4.*h])
+    ts = np.array(ts)
+    ys = np.array(ys)
     return (ys, ts)
 
 
