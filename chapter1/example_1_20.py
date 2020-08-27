e66d8fb202f4cc4fa30247e66038e8ec17c60c77
Jérémie Bannwarth <JBannwarth@users.noreply.github.com>
2020-08-27 13:37:08 +1200

    Add empty example 20

---
+++ chapter1/example_1_20.py
@@ -0,0 +1,7 @@
+""" Orbital Mechanics for Engineering Students Example 1.20
+Question:
+Written by: J.X.J. Bannwarth
+"""
+
+# Title
+print("Orbital Mechanics for Engineering Students Example 1.20")
