4b466f54c200a0d620d2e6a33448e7876af316a8
Jérémie Bannwarth <JBannwarth@users.noreply.github.com>
2020-08-28 23:14:43 +1200

    Complete Problem 1.2
    This is not an ideal way to display equations... Maybe use Jupyter for
    those types of questions?

--- chapter1/problem_1_2.py
+++ chapter1/problem_1_2.py
@@ -1,18 +1,13 @@
 """ Orbital Mechanics for Engineering Students Problem 1.2
 Question:
-Given four vectors A, B, C, and D and the identities from Problem 1.3
+Given four vectors A, B, C, and D and the identities from Problem 1.3, show
+    (A x B) . (C x D) = (A . C)(B . D) - (A . D)(B. C)
 Written by: J.X.J. Bannwarth
 """
 from sympy.vector import CoordSys3D
 from sympy.vector import Vector
 from sympy import symbols
 from sympy import simplify
-
-
-# Function
-def AreEqual(A, B):
-    return simplify(A - B).equals(Vector.zero)
-
 
 # Title
 print("Orbital Mechanics for Engineering Students Problem 1.2")
@@ -25,32 +20,25 @@ i, j, k = N.base_vectors()
 ax, ay, az = symbols("ax ay az")
 bx, by, bz = symbols("bx by bz")
 cx, cy, cz = symbols("cx cy cz")
+dx, dy, dz = symbols("dx dy dz")
 A = ax * i + ay * j + az * k
 B = bx * i + by * j + bz * k
 C = cx * i + cy * j + cz * k
+D = dx * i + dy * j + dz * k
 
-# (a)
-print("(a)")
-print("A . A =", A.dot(A))
-print("|A|**2 = ", A.magnitude()**2)
+# Analytical
+print("(1) Answer")
+print("(A x B) . (C x D)")
+print("A . [B x (C x D)] - interchangeability")
+print("A . [C(B . D) - D(B . C)] - bac-cab")
+print("(A . C)(B . D) - (A . D)(B . C) - expand")
 
-# (b)
-print("\n(b)")
-bLHS = A.dot(B.cross(C))
-bRHS = (A.cross(B)).dot(C)
-
-print("A . (B x C) = ", bLHS)
-print("(A x B) . C = ", bRHS)
-if simplify(bLHS - bRHS) == 0:
-    print("A . (B x C) == (A x B) . C")
-
-# (c)
-print("\n(c)")
-cLHS = simplify(A.cross(B.cross(C)))
-cRHS = simplify(B * A.dot(C) - C * A.dot(B))
-
-print("A x (B x C) = ", cLHS)
-print("B(A . C) - C(A . B) = ", cRHS)
-if AreEqual(cLHS, cRHS):
-    print("A x (B x C) == B(A . C) - C(A . B)")
+# Check the equations work out
+print("\n(2) Check")
+LHS = simplify((A.cross(B)).dot(C.cross(D)))
+RHS = simplify(A.dot(C) * B.dot(D) - A.dot(D) * B.dot(C))
+print("(A x B) . (C x D) =", LHS)
+print("(A . C)(B . D) - (A . D)(B . C) =", RHS)
+if simplify(LHS - RHS) == 0:
+    print("(A x B) . (C x D) == (A . C)(B . D) - (A . D)(B. C)")
 
