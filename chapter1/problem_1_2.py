2c6c5a185a5c15c1a1031881fb82e9eeb72c9ee0
Jérémie Bannwarth <JBannwarth@users.noreply.github.com>
2020-08-28 22:41:49 +1200

    Fix title

---
+++ chapter1/problem_1_2.py
@@ -0,0 +1,56 @@
+""" Orbital Mechanics for Engineering Students Problem 1.2
+Question:
+Given four vectors A, B, C, and D and the identities from Problem 1.3
+Written by: J.X.J. Bannwarth
+"""
+from sympy.vector import CoordSys3D
+from sympy.vector import Vector
+from sympy import symbols
+from sympy import simplify
+
+
+# Function
+def AreEqual(A, B):
+    return simplify(A - B).equals(Vector.zero)
+
+
+# Title
+print("Orbital Mechanics for Engineering Students Problem 1.2")
+
+# Coordinate system
+N = CoordSys3D('N')
+i, j, k = N.base_vectors()
+
+# Define vectors
+ax, ay, az = symbols("ax ay az")
+bx, by, bz = symbols("bx by bz")
+cx, cy, cz = symbols("cx cy cz")
+A = ax * i + ay * j + az * k
+B = bx * i + by * j + bz * k
+C = cx * i + cy * j + cz * k
+
+# (a)
+print("(a)")
+print("A . A =", A.dot(A))
+print("|A|**2 = ", A.magnitude()**2)
+
+# (b)
+print("\n(b)")
+bLHS = A.dot(B.cross(C))
+bRHS = (A.cross(B)).dot(C)
+
+print("A . (B x C) = ", bLHS)
+print("(A x B) . C = ", bRHS)
+if simplify(bLHS - bRHS) == 0:
+    print("A . (B x C) == (A x B) . C")
+
+# (c)
+print("\n(c)")
+cLHS = simplify(A.cross(B.cross(C)))
+cRHS = simplify(B * A.dot(C) - C * A.dot(B))
+
+print("A x (B x C) = ", cLHS)
+print("B(A . C) - C(A . B) = ", cRHS)
+if AreEqual(cLHS, cRHS):
+    print("A x (B x C) == B(A . C) - C(A . B)")
+
